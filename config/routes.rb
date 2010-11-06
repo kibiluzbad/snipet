b0ec2cc843108f6cdf19d1356e5f3733dcb727d0
Leonardo Cardoso <kibiluzbad@hotmail.com>
2010-11-06 12:25:28 -0200

    Correções.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Snipet::Application.routes.draw do
   devise_for :users
 
   root :to => "snippets#index"
-  get "snippets/mine" => 'snippets#mine'
+  get "snippets/mine" => 'snippets#mine', :as=>"mine_snippets"
   get 'languages',:controller => "languages", :action => "index"
   get "tags/(index)", :controller => "tags", :action => "index"
   resources :snippets  
